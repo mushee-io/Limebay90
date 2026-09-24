d8e9cea903f79417b1bc7e1a5d1bde0f0a003888
mushee <musheelabs@proton.me>
2026-09-24 16:26:10 +0100

    feat: build Nosh milestones 1-5

---
+++ lib/ultra/config.ts
@@ -0,0 +1,15 @@
+export const ULTRA_TESTNET_CHAIN_ID =
+  "7fc56be645bb76ab9d747b53089f132dcb7681db06f0852cfa03eaf6f7ac80e9";
+
+export const ULTRA_TESTNET_DEFAULT_RPCS = [
+  "https://ultra-testnet.eosphere.io",
+  "https://api.testnet.ultra.eossweden.org",
+  "https://testnet.ultra.eosrio.io",
+  "https://test.ultra.eosusa.io",
+  "https://api.ultra-testnet.cryptolions.io",
+] as const;
+
+export const NFT_CONTRACT = "eosio.nft.ft";
+export const TOKEN_TABLE = "token.b";
+export const FACTORY_TABLE = "factory.b";
+export const RESALE_TABLE = "resale.a";
